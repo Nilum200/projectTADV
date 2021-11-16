b1ab2d70d5f0c30bb640cb4ee264496390e216f2
Nilum <Robert_Schumacher@live.com>
2021-11-16 16:24:42 -0700

    ++ InputMethods class created with static methods: choose & scene_picker. ++ Learned how static methods work. ++ Praise be static methods.

--- main.py
+++ main.py
@@ -40,24 +40,56 @@ class PC:
         print("Your name is {name} and your gender pronouns are {gender}.".format(name=self.name, gender=self.gender))
 
 
+# Class hoards story beats, each contained in a function. Ideally I can automate the entire process.
+class StoryBeats(object):
+    @staticmethod
+    # Placeholder for the weapon scene.
+    def weapon_scene():
+        print("This is a weapon placeholder.")
+
+    # Placeholder for the window scene.
+    def window_scene():
+        print("This is a window placeholder.")
+
+
+# The class responsible for handling inputs.
+# Pedro is awesome.
+class InputMethods(object):
+    @staticmethod
+    # Give this a clearer name later.
+    # Takes input to choose the next scene, which is then put into the scene picker.
+    def choose(string_list):
+        valid = False
+        while not valid:
+            answer = input('Choose: ')
+            for item in string_list:
+                if item.lower() == answer.lower():
+                    valid = True
+                    print("Test Successful: " + answer + ".")
+                    # Eventually: This return statement returns a value fed into StoryBeats.
+                    scene_picker = answer.lower() + "_scene"
+                    return scene_picker
+            if not valid:
+                print("Invalid entry, please try again.")
+
+    # Scene picker checks the string input of a previous decision and then races off to fetch a scene.
+    def scene_picker(string):
+        if string == 'weapon_scene':
+            StoryBeats.weapon_scene()
+        elif string == 'window_scene':
+            StoryBeats.window_scene()
+        else:
+            print("Hmm...")
+
+
+first_choice = InputMethods.choose(['weapon', 'window'])
+InputMethods.scene_picker(first_choice)
+
 # Where things go until they're better organized. A test space, for testing... IN SPACE!
 
 # Establish an intro here. Eg: "{Name} finds {himself/herself} in danger..."
-player = PC()
-player.pc_intro()
+# player = PC()
+# player.pc_intro()
 
 # This is a lot to just vomit into python. I should really consider how best to design this so it isn't tons of lines.
-print("----------")
-print("{name} is a commoner, living in a small, idyllic village in a grand fantasy kingdom.".format(name=player.name))
-print("In recent times, people have been speaking of monsters who seek to assail the kingdom at large...")
-print("... but such must be mere fairy tales, right?")
-print("----------")
-print("{name} awakens to the scent of smoke and the sounds of screaming.".format(name=player.name))
-print("The town must be under attack!")
-print("{he} knows that somewhere in the house, his parents kept a weapon for self defense...".format(he=player.name))
-print("... but {he} would have to spend time trying to find it.".format(he=player.gender[0]))
-print("Alternatively, {he} could try to slip out the back window.".format(he=player.gender[0]))
-print("What to do...")
-print("1) Weapon \n 2) Window")
-Protagonist_Awakens = input("Select: ")
-print("You have chosen: " + Protagonist_Awakens)
+
